96b1f5c1c4ba997a5be7d79635088741a6ef85f6
sharadmohite <119413728+sharadmohite@users.noreply.github.com>
2025-02-16 21:36:05 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+class Example2
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number:");
+		int n=sc.nextInt();
+		int square=n*n;
+		System.out.println("square of the number is:"+square);
+	}
+
+}
